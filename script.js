484d528e4888930018335e6c64e965166922e9bd
Elitewebdigitals <62525712+Elitewebdigitals@users.noreply.github.com>
2020-12-12 04:51:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "ken" && b=="31stdec")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
